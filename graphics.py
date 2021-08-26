ba0ffae30e24689fe0191eee16fac983d62c48b8
ali-112 <alia-10-2001@hotmail.com>
2021-08-26 17:56:27 -0400

    added graphics file

--- graphics.py
+++ graphics.py
@@ -1,16 +1,18 @@
-from typing import runtime_checkable
 import pygame
 
+pygame.init()
 WIDTH, HEIGHT = 700, 900
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sudoku")
+
+WHITE = (255, 255, 255)
 
 run = True
 while run:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    
-
-    pygame.quit()
+            pygame.quit()
+        
+        WINDOW.fill(WHITE)
+        pygame.display.update()

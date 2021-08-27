1cf9115d53dfc7bcf94fa9d8d440d20045178b12
ali-112 <alia-10-2001@hotmail.com>
2021-08-26 20:36:12 -0400

    finished grid

--- graphics.py
+++ graphics.py
@@ -1,18 +1,71 @@
 import pygame
+from pygame.version import PygameVersion
+
+############## MAKE SURE LINES END AT COL 80?
 
 pygame.init()
-WIDTH, HEIGHT = 700, 900
+WIDTH, HEIGHT = 680, 780
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku")
 
-WHITE = (255, 255, 255)
+FPS = 60
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            pygame.quit()
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+def draw_window():
+    WINDOW.fill(WHITE)  # white area is (650 x 650) 
+    # bottom bar
+    pygame.draw.rect(WINDOW, BLACK, (0, HEIGHT-100, WIDTH, 100))
+    # border:
+    border_width = 15
+    box_width = WIDTH - 2*border_width #650
+    pygame.draw.rect(WINDOW, RED, (0, 0, border_width, HEIGHT-100)) #left
+    pygame.draw.rect(WINDOW, RED, (WIDTH-border_width, 0, border_width, HEIGHT-100)) #right
+    pygame.draw.rect(WINDOW, RED, (0, 0, WIDTH, border_width)) #top
+    pygame.draw.rect(WINDOW, RED, (0, HEIGHT-115, WIDTH, border_width)) #bottom
+    # big squares  (each square is 214 x 214)
+    bigline_width = 3
+    bigsquare_width = (box_width - 2*bigline_width) / 3  #214
+    pygame.draw.line(WINDOW, BLACK, (bigsquare_width+border_width, border_width), (bigsquare_width+border_width, box_width+border_width), bigline_width) #214 = (650-8)/3
+    pygame.draw.line(WINDOW, BLACK, (bigsquare_width*2+border_width+bigline_width, border_width), (bigsquare_width*2+border_width+bigline_width, box_width+border_width), bigline_width)
+    pygame.draw.line(WINDOW, BLACK, (border_width, bigsquare_width+border_width), (box_width+border_width, bigsquare_width+border_width), bigline_width)
+    pygame.draw.line(WINDOW, BLACK, (border_width, bigsquare_width*2+border_width+bigline_width), (box_width+border_width, bigsquare_width*2+border_width+bigline_width), bigline_width)
+    #### small squares (71.333 x 71.333)
+    # small vertical lines
+    smallsquare_width = bigsquare_width/3  #71.333
+    pygame.draw.line(WINDOW, BLUE, (smallsquare_width+border_width, border_width), (smallsquare_width+border_width, box_width+border_width))
+    pygame.draw.line(WINDOW, BLUE, (smallsquare_width*2+border_width+bigline_width, border_width), (smallsquare_width*2+border_width+bigline_width, box_width+border_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width+bigsquare_width+smallsquare_width, border_width), (border_width+bigsquare_width+smallsquare_width, border_width+box_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width+bigsquare_width+smallsquare_width*2+1, border_width), (border_width+bigsquare_width+smallsquare_width*2+1, border_width+box_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width+bigsquare_width*2+bigline_width*2+smallsquare_width, border_width), (border_width+bigsquare_width*2+bigline_width*2+smallsquare_width, border_width+box_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1, border_width), (border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1, border_width+box_width))
+    # small horizontal lines
+    pygame.draw.line(WINDOW, BLUE, (border_width, smallsquare_width+border_width), (border_width+box_width, smallsquare_width+border_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width, smallsquare_width*2+border_width+bigline_width), (border_width+box_width, smallsquare_width*2+border_width+bigline_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width+smallsquare_width), (border_width+box_width, border_width+bigsquare_width+smallsquare_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width+smallsquare_width*2+1), (border_width+box_width, border_width+bigsquare_width+smallsquare_width*2+1))
+    pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width), (border_width+box_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width))
+    pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1), (border_width+box_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1))
+
+
+
+    pygame.display.update()
+
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)  # controls the speed of this while loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
         
-        WINDOW.fill(WHITE)
-        pygame.display.update()
+        draw_window()
+        
+main()

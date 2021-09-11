6ec52cbf69ebab18befed3ea56a8c993324a2e17
ali-112 <alia-10-2001@hotmail.com>
2021-09-11 14:06:11 -0400

    added new grid & list consisting of the 81 squares

--- graphics.py
+++ graphics.py
@@ -36,8 +36,8 @@ def draw_window():
     # border:
     border_width = 10
     box_width = WIDTH - 2*border_width #650
-    grid = pygame.transform.scale(pygame.image.load('grid.png'), (660, 660))#(WIDTH-border_width*2-2, HEIGHT-border_width*2-100-2))
-    WINDOW.blit(grid, (border_width, border_width))
+    # grid = pygame.transform.scale(pygame.image.load('mygrid3.png'), (660, 660))#(WIDTH-border_width*2-2, HEIGHT-border_width*2-100-2))
+    WINDOW.blit(pygame.image.load('mygridnew.png'), (border_width, border_width)) #put grid for 1st param 
     pygame.draw.rect(WINDOW, RED, (0, 0, border_width, HEIGHT-100)) #left
     pygame.draw.rect(WINDOW, RED, (WIDTH-border_width, 0, border_width, HEIGHT-100)) #right
     pygame.draw.rect(WINDOW, RED, (0, 0, WIDTH, border_width)) #top
@@ -70,6 +70,153 @@ def draw_window():
     # pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width), (border_width+box_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width))
     # pygame.draw.line(WINDOW, BLUE, (border_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1), (border_width+box_width, border_width+bigsquare_width*2+bigline_width*2+smallsquare_width*2+1))
     
+
+
+
+    ###### THIS IS TO HIGHLIGHT A BOX
+    small_box_width = 69
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (border_width+5, border_width+5, 69, 69))
+
+    boxes_starting_corners = [(border_width+4+1, border_width+5), 
+    (border_width+4+1+69+1+1, border_width+5),
+    (border_width+4+1+70*2+3+1, border_width+5),
+    (border_width+4+1+72*3+2+1, border_width+5), 
+    (border_width+4+1+72*4+1, border_width+5), 
+    (border_width+4+1+72*5+2, border_width+5), 
+    (border_width+4+72*6+3*2, border_width+5), 
+    (border_width+4+72*7+5, border_width+5), 
+    (border_width+4+72*8+6, border_width+5),
+
+    (border_width+5, border_width+5+70), 
+    (border_width+5+71, border_width+5+70), 
+    (border_width+5+71*2+1, border_width+5+70), 
+    (border_width+4+1+72*3+2+1, border_width+5+70), 
+    (border_width+4+1+72*4+1, border_width+5+70), 
+    (border_width+4+1+72*5+2, border_width+5+70), 
+    (border_width+4+72*6+3*2, border_width+5+70), 
+    (border_width+4+72*7+5, border_width+5+70), 
+    (border_width+4+72*8+5, border_width+5+70),
+
+    (border_width+5, border_width+8+70*2), 
+    (border_width+5+71, border_width+8+70*2), 
+    (border_width+5+71*2+2, border_width+8+70*2), 
+    (border_width+4+1+72*3+2+1, border_width+8+70*2), 
+    (border_width+4+1+72*4+1, border_width+8+70*2), 
+    (border_width+4+1+72*5+2, border_width+8+70*2), 
+    (border_width+4+72*6+3*2, border_width+8+70*2), 
+    (border_width+4+72*7+5, border_width+8+70*2), 
+    (border_width+4+72*8+6, border_width+8+70*2),
+
+
+    (border_width+4+1, border_width+5+70*3+8), 
+    (border_width+4+1+69+1+1, border_width+5+70*3+8),
+    (border_width+4+1+70*2+3+1, border_width+5+70*3+8),
+    (border_width+4+1+72*3+2+1, border_width+5+70*3+8), 
+    (border_width+4+1+72*4+1, border_width+5+70*3+8), 
+    (border_width+4+1+72*5+2, border_width+5+70*3+8), 
+    (border_width+4+72*6+3*2, border_width+5+70*3+8), 
+    (border_width+4+72*7+5, border_width+5+70*3+8), 
+    (border_width+4+72*8+6, border_width+5+70*3+8),
+
+
+    (border_width+5, border_width+5+70*4+9), 
+    (border_width+5+71, border_width+5+70*4+9), 
+    (border_width+5+71*2+1, border_width+5+70*4+9), 
+    (border_width+4+1+72*3+2+1, border_width+5+70*4+9), 
+    (border_width+4+1+72*4+1, border_width+5+70*4+9), 
+    (border_width+4+1+72*5+2, border_width+5+70*4+9), 
+    (border_width+4+72*6+3*2, border_width+5+70*4+9), 
+    (border_width+4+72*7+5, border_width+5+70*4+9), 
+    (border_width+4+72*8+5, border_width+5+70*4+9),
+
+
+    (border_width+5, border_width+8+70*5+9), 
+    (border_width+5+71, border_width+8+70*5+9), 
+    (border_width+5+71*2+2, border_width+8+70*5+9), 
+    (border_width+4+1+72*3+2+1, border_width+8+70*5+9), 
+    (border_width+4+1+72*4+1, border_width+8+70*5+9), 
+    (border_width+4+1+72*5+2, border_width+8+70*5+9), 
+    (border_width+4+72*6+3*2, border_width+8+70*5+9), 
+    (border_width+4+72*7+5, border_width+8+70*5+9), 
+    (border_width+4+72*8+6, border_width+8+70*5+9),
+
+
+    (border_width+4+1, border_width+5+70*6+18), 
+    (border_width+4+1+69+1+1, border_width+5+70*6+18),
+    (border_width+4+1+70*2+3+1, border_width+5+70*6+18),
+    (border_width+4+1+72*3+2+1, border_width+5+70*6+18), 
+    (border_width+4+1+72*4+1, border_width+5+70*6+18), 
+    (border_width+4+1+72*5+2, border_width+5+70*6+18), 
+    (border_width+4+72*6+3*2, border_width+5+70*6+18), 
+    (border_width+4+72*7+5, border_width+5+70*6+18), 
+    (border_width+4+72*8+6, border_width+5+70*6+18),
+
+    (border_width+4+1, border_width+5+70*7+18), 
+    (border_width+4+1+69+1+1, border_width+5+70*7+18),
+    (border_width+4+1+70*2+3+1, border_width+5+70*7+18),
+    (border_width+4+1+72*3+2+1, border_width+5+70*7+18), 
+    (border_width+4+1+72*4+1, border_width+5+70*7+18), 
+    (border_width+4+1+72*5+2, border_width+5+70*7+18), 
+    (border_width+4+72*6+3*2, border_width+5+70*7+18), 
+    (border_width+4+72*7+5, border_width+5+70*7+18), 
+    (border_width+4+72*8+6, border_width+5+70*7+18),
+
+    (border_width+4+1, border_width+5+70*8+19), 
+    (border_width+4+1+69+1+1, border_width+5+70*8+19),
+    (border_width+4+1+70*2+3+1, border_width+5+70*8+19),
+    (border_width+4+1+72*3+2+1, border_width+5+70*8+19), 
+    (border_width+4+1+72*4+1, border_width+5+70*8+19), 
+    (border_width+4+1+72*5+2, border_width+5+70*8+19), 
+    (border_width+4+72*6+3*2, border_width+5+70*8+19), 
+    (border_width+4+72*7+5, border_width+5+70*8+19), 
+    (border_width+4+72*8+6, border_width+5+70*8+19)
+
+
+    # (border_width+5, border_width+6+3+69*3+8), 
+    # (border_width+4+1+72*3+2+1, border_width+6+3+69*3+8), 
+    # (border_width+4+72*6+3*2, border_width+6+3+69*3+8),
+    # (border_width+4+1, border_width+6+3+69*6+8*2+4),
+    # (border_width+4+1+72*3+2+1, border_width+6+3+69*6+8*2+4),
+    # (border_width+4+72*6+3*2, border_width+6+3+69*6+8*2+4)
+    ]
+    
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[1][0]+1, boxes_starting_corners[1][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[0][0], boxes_starting_corners[0][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[2][0]+1, boxes_starting_corners[2][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[3][0], boxes_starting_corners[3][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[4][0]+1, boxes_starting_corners[4][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[5][0]+1, boxes_starting_corners[5][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[6][0], boxes_starting_corners[6][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[7][0]+1, boxes_starting_corners[7][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[8][0]+1, boxes_starting_corners[8][1], 69+1, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[8][0]+71, boxes_starting_corners[8][1], 69+2, 69+1))
+    # pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[8][0]+71+1, boxes_starting_corners[8][1]+71*2+1, 69+1, 69+1))
+    # pygame.draw.line(WINDOW, GREEN, (25, border_width+6+69+1), (25, border_width+6+69*2), 5)
+    # pygame.draw.rect(WINDOW, GREEN, (border_width+5+70+1, boxes_starting_corners[0][1], 69, 69))
+    
+    
+    for i in range(len(boxes_starting_corners)):
+        
+        if i == 10 or i == 13 or i == 16 or i == 37 or i == 40 or i == 43:
+            # continue
+            pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[i][0], boxes_starting_corners[i][1], 69+4, 69+4))
+        if i % 3 == 1 and not (54 < i < 64):
+            # continue
+            pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[i][0], boxes_starting_corners[i][1], 69+4, 69+2))
+        if i == 55 or i == 58 or i == 61:
+            # continue
+            pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[i][0], boxes_starting_corners[i][1], 69+4, 69+1))
+        elif 8 < i < 18 or 35 < i < 45 or 62 < i < 72:
+            # continue
+            pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[i][0], boxes_starting_corners[i][1], 69+2, 69+4))
+        else:
+            # continue
+            pygame.draw.rect(WINDOW, (155, 155, 155), (boxes_starting_corners[i][0], boxes_starting_corners[i][1], 69+1, 69+1))
+        
+
+
+    pygame.draw.line(WINDOW, RED, (border_width+3+72*3, 20), (border_width+3+72*3+3, 20), 5)
+    pygame.draw.line(WINDOW, GREEN, (border_width, 20), (border_width+4, 20), 5)
     nums_font = pygame.font.SysFont('calibri', 50, False)
 
     for i in range(boardObj.size):
@@ -79,11 +226,11 @@ def draw_window():
                 nums_text = nums_font.render(str(boardObj.board[i][j]), 1, BLUE)
                 if (2<j<6):
                     # WINDOW.blit(nums_text, (17+24+j*72, 15+14+i*72))
-                    w = 17+24+j*72
+                    w = 15+24+j*72
                     h = 15+14+i*72
                 elif (5<j):
                     # WINDOW.blit(nums_text, (19+24+j*72.2, 15+14+i*72))
-                    w = 19+24+j*72.2
+                    w = 17+24+j*72.2
                     h = 15+14+i*72
                 else:
                     # WINDOW.blit(nums_text, (12+24+j*72.2, 15+14+i*72))
